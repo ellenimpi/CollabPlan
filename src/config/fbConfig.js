661bf4bb96817cc747cd7b9e1da573a5499826bb
Sun, Ellen <ellen.sun@imaginecommunications.com>
2019-07-03 18:29:58 -0400

    enter your own api key

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -4,13 +4,7 @@ import 'firebase/auth'
 
 
 var firebaseConfig = {
-    apiKey: "AIzaSyD1Ss626dDTfMno15ncwW6xpg7MG9DslvI",
-    authDomain: "the-project-planner.firebaseapp.com",
-    databaseURL: "https://the-project-planner.firebaseio.com",
-    projectId: "the-project-planner",
-    storageBucket: "",
-    messagingSenderId: "561765950228",
-    appId: "1:561765950228:web:7f44853415119c83"
+    //enter your own api key etc 
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
